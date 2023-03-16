3ee1e9c26a076ee9fadb7b9b987aeda4eef38c34
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-16 13:01:25 -0300

    feat: making sql more dynamic

--- sql/initial_sql.sql
+++ sql/initial_sql.sql
@@ -7,25 +7,45 @@ CREATE TABLE migrations (
     PRIMARY KEY (uuid)
 );
 
-CREATE TABLE groups (
-    uuid uuid DEFAULT uuid_generate_v4(),
-    name varchar(100) NOT NULL,
-    created_at timestamp DEFAULT NOW()
-);
+DO
+    $do$
+        BEGIN
+            IF (SELECT COUNT(*) <= 0 FROM migrations WHERE name = 'groups')
+                THEN
+                    CREATE TABLE groups (
+                        uuid uuid DEFAULT uuid_generate_v4(),
+                        name varchar(100) NOT NULL,
+                        created_at timestamp DEFAULT NOW()
+                    );
 
-CREATE TABLE users (
-    uuid uuid DEFAULT uuid_generate_v4(),
-    name varchar(255) NOT NULL,
-    cpf varchar(11) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    salt varchar(255) NOT NULL,
-    active boolean DEFAULT true,
-    created_at timestamp DEFAULT NOW(),
-    updated_at timestamp,
-    deleted_at timestamp,
-    PRIMARY KEY (uuid)
-);
+                    INSERT INTO migrations (name)
+                    VALUES ('groups');
+            END IF;
+        END;
+    $do$;
 
-INSERT INTO migrations (name)
-VALUES ('groups'), ('users');
+
+
+DO
+    $do$
+        BEGIN
+            IF (SELECT COUNT(*) <= 0 FROM migrations WHERE name = 'groups')
+                THEN
+                    CREATE TABLE users (
+                        uuid uuid DEFAULT uuid_generate_v4(),
+                        name varchar(255) NOT NULL,
+                        cpf varchar(11) NOT NULL,
+                        email varchar(255) NOT NULL,
+                        password varchar(255) NOT NULL,
+                        active boolean DEFAULT true,
+                        created_at timestamp DEFAULT NOW(),
+                        updated_at timestamp,
+                        deleted_at timestamp,
+                        PRIMARY KEY (uuid)
+                    );
+
+                    INSERT INTO migrations (name)
+                    VALUES ('users');
+            END IF;
+        END;
+    $do$;
