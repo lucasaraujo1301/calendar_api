683beb5048dbfacfebd7d0027f7b4a204ad93474
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-16 20:07:53 -0300

    feat: making login and returnin JWT token and expire him after 1 day

--- calendar_api/__init__.py
+++ calendar_api/__init__.py
@@ -1,6 +1,10 @@
 import os
 
+from datetime import timedelta
+
 from flask import Flask, g
+from flask_jwt_extended import JWTManager
+
 from calendar_api.blueprints import login
 from calendar_api.core import Core
 
@@ -10,8 +14,10 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DEBUG=True
+        JWT_SECRET_KEY='dev',
+        JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=1)
     )
+    JWTManager(app)
 
     # if test_config is None:
     #     # load the instance config, if it exists, when not testing
--- calendar_api/blueprints/login.py
+++ calendar_api/blueprints/login.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, g
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 app = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def register():
-    return 'Login API'
+    username = request.json.get("username", None)
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+@app.route('/protect')
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
