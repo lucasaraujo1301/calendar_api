04034740e8e7abeb90c78c05ec1e7c41652d6e53
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-16 14:22:49 -0300

    feat: creating the base to put core variable as global

--- calendar_api/blueprints/login.py
+++ calendar_api/blueprints/login.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, g
 
 app = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @app.route('/login')
 def register():
+    print(g.core)
     return 'Login API'
