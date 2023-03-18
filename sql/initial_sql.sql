6729dc2e4b3ac761ec34ae9835769128d2c16e3c
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-18 18:41:21 -0300

    feat: adding foreign key from groups in users table

--- sql/initial_sql.sql
+++ sql/initial_sql.sql
@@ -13,7 +13,7 @@ DO
             IF (SELECT COUNT(*) <= 0 FROM migrations WHERE name = 'groups')
                 THEN
                     CREATE TABLE groups (
-                        uuid uuid DEFAULT uuid_generate_v4(),
+                        uuid uuid DEFAULT uuid_generate_v4() UNIQUE,
                         name varchar(100) NOT NULL,
                         created_at timestamp DEFAULT NOW()
                     );
@@ -29,19 +29,23 @@ DO
 DO
     $do$
         BEGIN
-            IF (SELECT COUNT(*) <= 0 FROM migrations WHERE name = 'groups')
+            IF (SELECT COUNT(*) <= 0 FROM migrations WHERE name = 'users')
                 THEN
                     CREATE TABLE users (
-                        uuid uuid DEFAULT uuid_generate_v4(),
+                        uuid uuid DEFAULT uuid_generate_v4() UNIQUE,
                         name varchar(255) NOT NULL,
-                        cpf varchar(11) NOT NULL,
-                        email varchar(255) NOT NULL,
+                        cpf varchar(11) NOT NULL UNIQUE,
+                        email varchar(255) NOT NULL UNIQUE,
                         password varchar(255) NOT NULL,
                         active boolean DEFAULT true,
+                        group_uuid uuid,
                         created_at timestamp DEFAULT NOW(),
                         updated_at timestamp,
                         deleted_at timestamp,
-                        PRIMARY KEY (uuid)
+                        PRIMARY KEY (uuid),
+                        CONSTRAINT fk_user_groups
+                            FOREIGN KEY (group_uuid)
+                                REFERENCES groups(uuid)
                     );
 
                     INSERT INTO migrations (name)
