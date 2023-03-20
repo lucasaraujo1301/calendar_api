ebb98e599a80ffbde1beef111386057d56c5d0a4
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-20 18:26:23 -0300

    feat: finishing the github action config

--- calendar_api/utils.py
+++ calendar_api/utils.py
@@ -7,6 +7,6 @@ path = os.path.dirname(__file__)
 
 
 def get_config_secrets():
-    config_file = os.environ['ENVIRONMENT_CONFIG']
+    config_file = os.getenv('ENVIRONMENT_CONFIG', 'local_config')
     with open(os.path.join(path, f'config/{config_file}.yaml'), 'r') as f:
         return yaml.load(f, Loader=SafeLoader)
