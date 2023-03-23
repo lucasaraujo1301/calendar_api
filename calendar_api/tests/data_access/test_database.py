0d44a0726ae8190b988900ad74890ce0cf9e5aff
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-03-23 12:26:46 -0300

    fix: updating the test_database to pass the pipeline

--- calendar_api/tests/data_access/test_database.py
+++ calendar_api/tests/data_access/test_database.py
@@ -10,18 +10,18 @@ class TestDatabase(TestCase):
         self.db = Database(self.config_secrets['connect_db'], 'calendar_api')
 
     def test_connect_db(self):
-        assert self.db.connect_db(self.config_secrets['connect_db'], 'calendar_api') is not None
+        assert self.db.connect_db(self.config_secrets['connect_db']) is not None
         assert self.db.connection_string is not {}
         assert self.db._db is not None
 
     def test_close_db(self):
-        self.db.connect_db(self.config_secrets['connect_db'], 'calendar_api')
+        self.db.connect_db(self.config_secrets['connect_db'])
         self.db.close_db()
         assert getattr(self.db._db, 'closed') > 0
 
     def test_get_db(self):
         # Testing with a open database
-        self.db.connect_db(self.config_secrets['connect_db'], 'calendar_api')
-        assert self.db.get_db('calendar_api') is not None
+        self.db.connect_db(self.config_secrets['connect_db'])
+        assert self.db.get_db() is not None
         assert self.db.connection_string
         assert self.db._db is not None
